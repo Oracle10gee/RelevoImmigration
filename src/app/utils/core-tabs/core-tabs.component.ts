2339db33f04dbaa4fbb9004aba844d1375cd4e16
Ayedun Olakunle <Oracle10gee@gmail.com>
2023-08-08 21:43:36 +0100

    transfer modal

--- src/app/utils/core-tabs/core-tabs.component.ts
+++ src/app/utils/core-tabs/core-tabs.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { ModalContentComponent } from '../../@components/admin-dashboard/dashboard-home/modal-content/modal-content.component';
 
 @Component({
   selector: 'app-core-tabs',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class CoreTabsComponent {
   coreActiveTab: string = 'coreTab1';
+
+  constructor(private modalService: NgbModal) {}
+
+  isBalanceHidden = true;
+
+  toggleBalance(): void {
+    this.isBalanceHidden = !this.isBalanceHidden;
+  }
+
+  openModal() {
+    const modalOptions: NgbModalOptions = {
+      backdrop: 'static',
+      keyboard: false,
+      size: 'md', // Set the size of the modal (e.g., 'lg', 'sm')
+    };
+
+    const modalRef = this.modalService.open(
+      ModalContentComponent,
+      modalOptions
+    );
+  }
 }
